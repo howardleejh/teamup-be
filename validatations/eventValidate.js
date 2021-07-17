9302c2db277461833100bc2ec448bc55f14424ef
Howard Lee <howardlee@Howards-MacBook-Pro.local>
2021-07-18 03:12:23 +0800

    added ISO time format

--- validatations/eventValidate.js
+++ validatations/eventValidate.js
@@ -6,7 +6,7 @@ module.exports = {
   createEventValidator: Joi.object({
     event_name: Joi.string().min(3).required(),
     from: Joi.date().iso().greater('now').required(),
-    to: Joi.date().greater(Joi.ref('from')).required(),
+    to: Joi.date().iso().greater(Joi.ref('from')).required(),
     location: Joi.string().min(3),
     description: Joi.string().max(300).empty(''),
   }),
