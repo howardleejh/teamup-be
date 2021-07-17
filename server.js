fcb3c85a4585237b526bb5b00aed339d4f525315
Howard Lee <howardlee@Howards-MacBook-Pro.local>
2021-07-17 10:09:45 +0800

    added port number for heroku

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const guestsRouter = require('./routers/guestsRouter')
 const todoController = require('./controllers/todoController')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // =====================================
 //          Using Middleware
