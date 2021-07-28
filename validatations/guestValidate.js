41b904f2bec47012d9af88cb11c9aa40e472b13d
Howard Lee <howardlee@Howards-MacBook-Pro.local>
2021-07-28 21:23:46 +0800

    change guest contact to validate only number integers

--- validatations/guestValidate.js
+++ validatations/guestValidate.js
@@ -5,7 +5,7 @@ const Joi = require('joi')
 module.exports = {
   createGuestValidator: Joi.object({
     guest_fullName: Joi.string().min(3).max(50).required(),
-    guest_contact: Joi.string().alphanum().max(8),
+    guest_contact: Joi.number().integer().required(),
     role: Joi.string().valid('bride', 'groom').required(),
     status: Joi.string()
       .valid('pending', 'attending', 'unavailable')
